641f4fec4dca7c7b428c68aacfb5e77861943dc6
csanchez82 <c.sanchez@cirb.edu.mx>
2024-11-17 15:49:42 -0600

    Deshabilitado eliminar demo

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -63,19 +63,30 @@
                                         </svg>
                                     </span>
                                 @endif
-                                <button type="button" class="text-red-600 hover:text-red-900" onclick="confirmDeletion('{{ $task->id }}')" title="Eliminar">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M3 6h18"/>
-                                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
-                                        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
-                                        <line x1="10" x2="10" y1="11" y2="17"/>
-                                        <line x1="14" x2="14" y1="11" y2="17"/>
-                                    </svg>
-                                </button>
-                                <form id="delete-form-{{ $task->id }}" action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display: none;">
-                                    @csrf
-                                    @method('DELETE')
-                                </form>
+                                @php
+    $isDemo = config('app.demo_mode');
+@endphp
+
+<button 
+    type="button" 
+    class="text-red-600 hover:text-red-900 {{ $isDemo ? 'cursor-not-allowed opacity-50' : '' }}" 
+    onclick="{{ !$isDemo ? "confirmDeletion('$task->id')" : '' }}" 
+    title="Eliminar" 
+    {{ $isDemo ? 'disabled' : '' }}
+>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M3 6h18"/>
+        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+        <line x1="10" x2="10" y1="11" y2="17"/>
+        <line x1="14" x2="14" y1="11" y2="17"/>
+    </svg>
+</button>
+<form id="delete-form-{{ $task->id }}" action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display: none;">
+    @csrf
+    @method('DELETE')
+</form>
+
                             </td>
                         </tr>
                     @endforeach
